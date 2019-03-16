085842aa05c313cfe2bd02a4d728ea180927cec8
Leif Eriksen <leif.eriksen.au@gmail.com>
2019-03-17 00:23:22 +1100

    use snapshot and nock-record

--- __test__/fetchSteam.test.ts
+++ __test__/fetchSteam.test.ts
@@ -1,29 +1,20 @@
 import "jest";
 
-import * as nock from "nock";
-import fetch from "node-fetch";
+import { setupRecorder } from "nock-record";
+import { getData } from "../src/fetchSteam";
+// import {defaultOptions} from '../helpers/nock';
 
-import { getData, SteamPriceData } from "../src/fetchSteam";
+const record = setupRecorder();
 
 describe("fetchSteam", () => {
     describe("getData", () => {
-        afterAll(() => nock.restore());
-        afterEach(() => nock.cleanAll());
 
         it("gets data from Steam", async () => {
-            nock(`http://steamcommunity.com`)
-            .get(/^\/market\/priceoverview\/\?appid=\d+&country=DE&currency=3&market_hash_name=GAMESCOM%20INVITATIONAL%20CRATE$/)
-            .reply(
-                200,
-                {
-                    lowest_price: "0,21€",
-                    median_price: "0,20€",
-                    success: true,
-                    volume: "250"
-                }
-            );
-
-            return expect(await getData('578080', 'GAMESCOM INVITATIONAL CRATE')).toMatchSnapshot();
+            const {completeRecording, assertScopesFinished} = await record("steam-price-data");
+            const result = await getData('578080', 'GAMESCOM INVITATIONAL CRATE');
+            completeRecording();
+            assertScopesFinished();
+            expect(result).toMatchSnapshot();
         });
     });
 });
