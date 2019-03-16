b24a83396b9195b53d3e56cfa0918714ee336353
Leif Eriksen <leif.eriksen.au@gmail.com>
2019-03-16 20:25:29 +1100

    move mock to test

--- __test__/fetchSteam.test.ts
+++ __test__/fetchSteam.test.ts
@@ -10,9 +10,9 @@ describe("fetchSteam", () => {
         afterAll(() => nock.restore());
         afterEach(() => nock.cleanAll());
 
-        beforeAll(() => {
+        it("gets data from Steam", async () => {
             nock(`http://steamcommunity.com`)
-            .get(/^\/market\/priceoverview\/\?appid=578080&country=DE&currency=3&market_hash_name=GAMESCOM%20INVITATIONAL%20CRATE$/)
+            .get(/^\/market\/priceoverview\/\?appid=\d+&country=DE&currency=3&market_hash_name=GAMESCOM%20INVITATIONAL%20CRATE$/)
             .reply(
                 200,
                 {
@@ -22,9 +22,7 @@ describe("fetchSteam", () => {
                     volume: "250"
                 }
             );
-        });
 
-        it("gets data from Steam", async () => {
             return expect(await getData('578080', 'GAMESCOM INVITATIONAL CRATE')).toMatchSnapshot();
         });
     });
