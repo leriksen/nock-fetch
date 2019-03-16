b0b4c4bf97dbc11ee554c2cc7644e12fd2130faf
Leif Eriksen <leif.eriksen.au@gmail.com>
2019-03-17 00:53:09 +1100

    multiple tests in one suite need separate recording files

--- __test__/githubProfile.test.ts
+++ __test__/githubProfile.test.ts
@@ -5,12 +5,19 @@ import { getData } from "../src/githubProfile";
 
 const record = setupRecorder();
 
-describe("githubPrfile", () => {
+describe("githubProfile", () => {
     describe("getData", () => {
-
         it("gets data on user from Github", async () => {
-            const {completeRecording, assertScopesFinished} = await record("github-data");
+            const {completeRecording, assertScopesFinished} = await record("github1-data");
             const result = await getData('leriksen');
+            completeRecording();
+            assertScopesFinished();
+            expect(result).toMatchSnapshot();
+        });
+        
+        it("gets data on another user from Github", async () => {
+            const {completeRecording, assertScopesFinished} = await record("github2-data");
+            const result = await getData('edorivai');
             completeRecording();
             assertScopesFinished();
             expect(result).toMatchSnapshot();
