d027a51065efe4546679b90d87baf86752b528a1
Leif Eriksen <leif.eriksen.au@gmail.com>
2019-12-25 13:39:07 +1100

    add html, junit and coverage reports to the testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    'jest-junit',
+    [
+      'jest-stare',
+      {
+        "coverageLink": "../coverage/lcov-report/index.html",
+      }
+    ]
+ ],
+  collectCoverage: true
 };
